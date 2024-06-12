a49ceaaac0230690d42ee3e31437034d6ee5079a
Htgbot <124916759+Htgbot@users.noreply.github.com>
2024-06-12 09:50:43 +0530

    Update renamer.py

--- renamer.py
+++ renamer.py
@@ -1,8 +1,9 @@
 import json
 import os
+from shutil import copy2
 
-# Function to rename images based on JSON data and save them to a new folder
-def rename_images(json_file, image_folder, new_folder):
+# Function to rename images based on JSON data and copy them to a new folder
+def rename_and_copy_images(json_file, image_folder, new_folder):
     # Load the JSON file
     with open(json_file, 'r') as file:
         data = json.load(file)
@@ -25,20 +26,20 @@ def rename_images(json_file, image_folder, new_folder):
         if os.path.exists(current_image_path):
             # Check if the new image name does not exist in the new folder
             if not os.path.exists(new_image_path):
-                # Rename and move the image to the new folder
-                os.rename(current_image_path, new_image_path)
-                print(f"Renamed {current_image_name} to {new_image_name} and moved to {new_folder}")
+                # Copy the image to the new folder with the new name
+                copy2(current_image_path, new_image_path)
+                print(f"Copied {current_image_name} to {new_image_name} in {new_folder}")
             else:
-                print(f"Cannot move {current_image_name} to {new_folder} because {new_image_name} already exists there.")
+                print(f"Cannot copy {current_image_name} to {new_folder} because {new_image_name} already exists there.")
         else:
             print(f"Image {current_image_name} not found in {image_folder}.")
 
-    print('Image renaming and moving process completed.')
+    print('Image renaming and copying process completed.')
 
 # Specify the JSON file, the folder where the images are currently stored, and the new folder
 json_file = 'alldat.json'
 image_folder = 'webp'
 new_folder = 'webpimg'
 
-# Call the function to start renaming images and moving them to the new folder
-rename_images(json_file, image_folder, new_folder)
+# Call the function to start renaming images and copying them to the new folder
+rename_and_copy_images(json_file, image_folder, new_folder)
